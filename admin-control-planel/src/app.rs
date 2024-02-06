a84c892d03b4487127eec4b18368278a4fc57b47
Serveny <dylan_lewis93@web.de>
2024-02-06 07:28:31 +0100

    admin panel WIP

--- admin-control-planel/src/app.rs
+++ admin-control-planel/src/app.rs
@@ -1,5 +1,7 @@
 use crate::SERVER_URL;
 use gloo_net::http::Request;
+use gloo_net::Error;
+use serde::de::DeserializeOwned;
 use uuid::Uuid;
 use yew::prelude::*;
 use yew::suspense::use_future;
@@ -7,34 +9,56 @@ use yew::suspense::use_future;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            <Rooms></Rooms>
-        </main>
+        <>
+            <NavBar/>
+            <main>
+                <Suspense fallback={html!{<div aria-busy="true" class="secondary"></div>}}>
+                    <Rooms/>
+                </Suspense>
+            </main>
+        </>
+    }
+}
+
+#[function_component]
+fn NavBar() -> Html {
+    html! {
+        <nav>
+            <ul>
+                <li><strong>{ "Web Imgage Manager" }</strong></li>
+            </ul>
+            <div>
+                <li><a href="#" role="button" class="secondary outline">{ "Images" }</a></li>
+                <li><a href="#" role="button" class="secondary outline">{ "Statistics" }</a></li>
+            </div>
+        </nav>
     }
 }
 
 #[function_component]
 fn Rooms() -> HtmlResult {
-    let res = use_future(|| async {
-        Request::get(&format!(
-            "{SERVER_URL}/list/6a766d31-71d5-4a34-8df5-124b9614b19f"
-        ))
-        .send()
-        .await?
-        .text()
-        .await
-    })?;
-    let res: &String = match *res {
+    let res = use_future(|| async { get_json("list/6a766d31-71d5-4a34-8df5-124b9614b19f").await })?;
+    let res: &Vec<Uuid> = match *res {
         Ok(ref res) => res,
         Err(ref err) => return Ok(html! { err.to_string() }),
     };
-    let res: Vec<Uuid> = match serde_json::from_str(res) {
-        Ok(res) => res,
-        Err(err) => return Ok(html! { err.to_string() }),
-    };
 
-    Ok(html! {})
+    Ok(html! {
+        <article>
+        { for res.iter().map(|item| html! {
+            <details>
+                <summary>{item.to_string()}</summary>
+            </details>
+        }) }
+        </article>
+    })
+}
+
+async fn get_json<T: DeserializeOwned>(url: &str) -> Result<T, Error> {
+    Request::get(&format!("{SERVER_URL}/{url}"))
+        .header("Accept", "application/json")
+        .send()
+        .await?
+        .json::<T>()
+        .await
 }
--- admin-control-planel/src/main.rs
+++ admin-control-planel/src/main.rs
@@ -2,7 +2,7 @@ mod app;
 
 use app::App;
 
-const SERVER_URL: &str = "";
+const SERVER_URL: &str = "http://127.0.0.1:8080";
 
 fn main() {
     yew::Renderer::<App>::new().render();
